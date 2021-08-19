890850c7e25fdeaaf70207d6333f31a91c3b7d39
Guy Hunt <guypwhunt1992@gmail.com>
2021-08-19 13:51:27 +0100

    Automated MACD update

--- test.R
+++ test.R
@@ -1,11 +1,9 @@
 source("functions.R")
 
-df <- read.csv("indexFund.csv")
+#df <- read.csv("indexFund.csv")
 #df <- read.csv("industryEtfs.csv", fileEncoding="UTF-8-BOM")
 #df <- read.csv("geographicalEtfs.csv")
-#df <- read.csv("ftse100Companies.csv", fileEncoding="UTF-8-BOM")
-
-df[df$ID == list("0P00011UPP.L", "0P0000WN7M.L"),]
+df <- read.csv("ftse100Companies.csv", fileEncoding="UTF-8-BOM")
 
 finalDf <- df
 #rbind(rbind(df["ID"], df1["ID"]), df2["ID"])
@@ -26,7 +24,8 @@ for (time in timeSpan) {
       }
     }, silent = TRUE)
   }
+  print("")
   print(time)
-  print(symbols)
-  df[df$ID == symbols,]
+  subDf <- df[df$ID %in% symbols,]
+  print(subDf[, c("ID", "Name")])
 }
